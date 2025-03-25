030ee857deb6e68b29504bf8f5c675530f433e33
Khoa Le <lequanganhkhoa2005@gmail.com>
2025-03-25 16:13:08 +0700

    format_data

--- Crawl_YTB/save_utils.py
+++ Crawl_YTB/save_utils.py
@@ -1,17 +1,35 @@
+from google.cloud import storage
 import os
+from datetime import datetime,timezone
+import time
 import json
 
-def append_to_json(filename, new_data):
-    if os.path.exists(filename):
-        with open(filename, "r", encoding="utf-8") as f:
-            old_data = json.load(f)
-    else:
-        old_data = []
-
-    old_ids = {item.get("channel_id") or item.get("video_id") for item in old_data}
-    filtered = [item for item in new_data if (item.get("channel_id") or item.get("video_id")) not in old_ids]
-    combined_data = old_data + filtered
-
+def append_to_json(filename, new_data ,namebucket):
+    """Ghi dữ liệu vào file JSON, luôn tạo file mới"""
     with open(filename, "w", encoding="utf-8") as f:
-        json.dump(combined_data, f, ensure_ascii=False, indent=2)
-    print(f"Đã thêm {len(filtered)} dòng mới vào {filename}")
+        json.dump(new_data, f, ensure_ascii=False, indent=2)
+    print(f"Đã ghi {len(new_data)} dòng vào {filename}")
+    file_path=os.path.abspath(filename)
+    upload_to_gcs( namebucket , file_path)
+
+def upload_to_gcs(bucket_name, file_path):
+    """Upload file lên GCS theo định dạng YouTube/year=yyyy/month=mm/day=dd/YouTube_datatype_timestamp.json"""
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+
+    # Lấy thời gian hiện tại
+    now = datetime.now(timezone.utc)
+    year = now.strftime("%Y")
+    month = now.strftime("%m")
+    day = now.strftime("%d")
+    timestamp = int(time.time())  # Timestamp hiện tại (tính bằng giây)
+
+    # Tạo blob_name theo format yêu cầu
+    blob_name = f"youtube/year={year}/month={month}/day={day}/youtube_{"channel_info"}_{timestamp}.json"
+
+    # Tạo blob và upload
+    blob = bucket.blob(blob_name)
+    blob.upload_from_filename(file_path)
+
+    print(f"Đã upload {file_path} lên gs://{bucket_name}/{blob_name} thành công.")
+    return f"gs://{bucket_name}/{blob_name}"
