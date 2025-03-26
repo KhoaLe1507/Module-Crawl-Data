83fb260e6899abc28adedaf06d4835006dd37444
dusk5archiy <dusqdn.eml@gmail.com>
2025-03-26 21:14:35 +0700

    uploaded to gcs successfully

--- main.py
+++ main.py
@@ -9,22 +9,36 @@ from src.utils.config import Config
 from src.gcs.secret import GcsSecret
 
 import time
+import os
 
 
 def load_configs():
     with open("config.json", "r") as f:
         configs = json.load(f)
 
-    Config.set_configs(configs)
+    Config.input_folder = configs["input_folder"]
+    Config.output_folder = configs["output_folder"]
+    Config.secret_folder = configs["secret_folder"]
+    Config.bucket_name = configs["bucket_name"]
+    Config.product_mode = configs["product_mode"]
+    Config.project_id = configs["project_id"]
 
 
-def main():
+def head():
     load_configs()
+    if Config.product_mode:
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.abspath(
+            f"{Config.secret_folder}/creator-dev-453406-dcbfdc47e0e1.json"
+        )
+
+
+def body():
     gcs_secret = None
     if Config.product_mode:
         gcs_secret = GcsSecret()
     starttime = time.time()
-    scrape_facebook(gcs_secret)
+    if not Config.product_mode:
+        scrape_facebook(gcs_secret)
     scrape_instagram()
     scrape_tiktok()
     scrape_youtube(gcs_secret)
@@ -33,6 +47,7 @@ def main():
 
 
 def gcs_main(request):
+    head()
     auth_header = request.headers.get("Authorization")
     if not auth_header:
         return (
@@ -46,8 +61,15 @@ def gcs_main(request):
     if not token:
         return "Invalid token", 403  # Nếu không có token, trả về lỗi
 
-    main()
+    body()
+
+
+def main():
+    head()
+    body()
 
 
 if __name__ == "__main__":
     main()
+
+# EOF
--- src/utils/config.py
+++ src/utils/config.py
@@ -6,11 +6,5 @@ class Config:
     bucket_name: str
     project_id: str
 
-    @classmethod
-    def set_configs(cls, configs):
-        cls.input_folder = configs["input_folder"]
-        cls.output_folder = configs["output_folder"]
-        cls.secret_folder = configs["secret_folder"]
-        cls.bucket_name = configs["bucket_name"]
-        cls.product_mode = configs["product_mode"]
-        cls.project_id = configs["project_id"]
+
+# EOF
